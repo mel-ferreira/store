548c07965eb7e05c4d8daae1be306954848f59ab
mel-ferreira <melissamouraf@gmail.com>
2026-02-27 13:06:07 -0300

    [FEATURE] Insert do Usuario

--- src/main/java/procart/store/resources/UserResources.java
+++ src/main/java/procart/store/resources/UserResources.java
@@ -2,13 +2,12 @@ package procart.store.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import procart.store.entities.User;
 import procart.store.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,6 +28,16 @@ public class UserResources {
     {
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @PostMapping
+    public ResponseEntity<User> insertUsuario(@RequestBody User obj)
+    {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").
+                buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+
     }
 
 }
